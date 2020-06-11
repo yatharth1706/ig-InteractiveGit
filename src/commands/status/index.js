47c4e88bb9ac16ade5e77ce23ddabc22d1f4ad96
yatharth <yatharthverma070@gmail.com>
2020-06-11 17:18:09 +0530

    :pencil: - Fix status command

--- src/commands/status/index.js
+++ src/commands/status/index.js
@@ -1,5 +1,21 @@
+const execa = require('execa');
+const chalk = require('chalk');
+const parseGitStatus = require('parse-git-status');
+
 function status() {
-    console.log("Init");
+    try{
+        (
+            async () => {
+                const {stdout} = await execa('git', ['status', '--porcelain']);
+                const output = parseGitStatus(stdout);
+                console.log(output[0]['to']);
+                console.log(parseGitStatus.describeCode('MM'));
+            }
+        )();
+    }catch(error){
+        console.log(chalk.red(error));
+    }
+    
 }
 
 module.exports = {
