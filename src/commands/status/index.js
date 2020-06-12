7dc52138059afd9eabecdd50de0b74ddf61dde7d
yatharth <yatharthverma070@gmail.com>
2020-06-12 16:12:32 +0530

    :zap: - Add status command

--- src/commands/status/index.js
+++ src/commands/status/index.js
@@ -1,20 +1,47 @@
 const execa = require('execa');
 const chalk = require('chalk');
 const parseGitStatus = require('parse-git-status');
+const inquirer = require('inquirer');
+
+
+const questions = [
+    {
+        type: 'list',
+        name: 'statusType',
+        message: 'Choose Type of Status',
+        choices: ['short','long','porcelain v1','porcelain v2']
+    }
+]
 
 function status() {
-    try{
-        (
-            async () => {
-                const {stdout} = await execa('git', ['status', '--porcelain']);
-                const output = parseGitStatus(stdout);
-                console.log(output[0]['to']);
-                console.log(parseGitStatus.describeCode('MM'));
-            }
-        )();
-    }catch(error){
-        console.log(chalk.red(error));
-    }
+    inquirer.prompt(questions).then((answers) => {
+        
+        let flag = "";
+        if(answers.statusType == 'short'){
+            flag = '-s';
+        }else if(answers.statusType == 'long'){
+            flag = '--long';
+        }else if(answers.statusType == 'porcelain v1'){
+            flag = '--porcelain=v1';
+        }else if(answers.statusType == 'porcelain v2'){
+            flag = '--porcelain=v2';
+        }
+
+        try{
+            (
+                async () => {
+                    const {stdout} = await execa('git', ['status', flag], {
+                        stdio: 'inherit'
+                    });
+                    
+                }
+            )();
+        }catch(error){
+            console.log(chalk.red(error));
+        }
+
+    })
+    
     
 }
 
